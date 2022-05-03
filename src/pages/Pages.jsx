5c401c605a3e4511e2b3a6dc99e81f6d244a97b2
saket105 <saketcool35@gmail.com>
2022-05-03 21:02:00 +0530

    saket

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,13 +3,14 @@ import Home from "./Home";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import { Route, Routes } from "react-router-dom";
-import Search from "../components/Search";
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/cuisine/:type" element={<Cuisine />} />
+
       <Route path="/searched/:search" element={<Searched />} />
     </Routes>
   );
