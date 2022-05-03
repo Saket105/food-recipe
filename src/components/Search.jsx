9e7b8a42634641901e10ebc6323bbeb57868cc76
saket105 <saketcool35@gmail.com>
2022-05-03 22:53:10 +0530

    searching complete

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
+    navigate("/searched/" + input);
   };
 
   return (
@@ -18,7 +21,7 @@ function Search() {
           type="text"
           value={input}
         />
-        <h4>{input}</h4>
+        {/* <h4>{input}</h4> */}
       </div>
     </FormStyle>
   );
